ec462f2dd5f64231c8b737314fd942710c839326
Rohan <rohanjaiaprasat@gmail.com>
2024-06-03 15:37:36 +0100

    add and run loader func

--- stages/1.prepare/scrape.py
+++ stages/1.prepare/scrape.py
@@ -3,6 +3,7 @@ import json
 import pandas as pd
 import requests as rs
 from pathlib import Path
+from typing import List
 
 def fetch(api: str, pages: int, path: str) -> None:
     """
@@ -40,10 +41,68 @@ def fetch(api: str, pages: int, path: str) -> None:
             print(f"Failed to fetch data for page {page + 1}: {e}")
             continue
 
+def load_and_process(path: str) -> pd.DataFrame:
+    """
+    Load data from JSON files, process it, and return a DataFrame.
+    
+    Args:
+    path(str): The path from which JSON files will be loaded.
+    
+    Returns:
+    pd.DataFrame: DataFrame containing the processed data.
+    
+    Raises:
+    ValueError: If the data path is not a string or is empty.
+    """
+
+    if not isinstance(path, Path) or not path:
+        raise ValueError("The data path must be a non-empty string.")
+    
+    properties_list: List[dict] = []
+
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
+        if not os.path.isfile(file_path):
+            print(f"Skipping non-file {filename}")
+            continue
+        with open(file_path) as file:
+            try:
+                data = json.load(file)  # Load JSON data from file
+
+                for property in data.get("properties", []):
+                    landlord = property.get("landlord", {})
+                    property_data = {
+                        "id": property.get("id"),
+                        "lat": property.get("latitude"),
+                        "long": property.get("longitude"),
+                        "postcode": property.get("postcode"),
+                        "price": property.get("price"),
+                        "rooms": property.get("roomsAvailableCount"),
+                        "images": property.get("imagesCount"),
+                        "type": property.get("type"),
+                        "addressShort": property.get("addressShort"),
+                        "street": property.get("street"),
+                        "landlord_id": landlord.get("id"),
+                        "fname": landlord.get("first_name"),
+                        "sname": landlord.get("last_name"),
+                        "company": landlord.get("company"),
+                        "added": property.get("added")
+                    }
+                    properties_list.append(property_data)  # Add property data to list
+            except json.JSONDecodeError as e:
+                print(f"Failed to load JSON data from {filename}: {e}")
+                continue
+
+    df = pd.DataFrame(properties_list)  # Create DataFrame from list
+    return df
+
 def main() -> None:
     DATA_DIR: str = Path.cwd().joinpath("stages","1.collect","data")
-    api: str = "https://www.unihomes.co.uk/student-accommodation/leicester?page={}&reload=true"
-    fetch(api, 14, DATA_DIR)
-    
+    #api: str = "https://www.unihomes.co.uk/student-accommodation/leicester?page={}&reload=true"
+    #fetch(api, 14, DATA_DIR)
+    df = load_and_process(DATA_DIR)
+    print(df.head(10))
+    df.to_csv("house.csv", index=False)
+        
 if __name__ == "__main__":
     main()
